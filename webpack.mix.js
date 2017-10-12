262de1c4239f6809f6a48ab33bd87aac5a6aa098
Dan Pastori <dan@521dimensions.com>
2017-10-12 12:28:51 -0700

    Production push

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,20 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
+    .webpackConfig({
+         module: {
+             rules: [
+                 {
+                     test: /\.jsx?$/,
+                     exclude: /node_modules(?!\/foundation-sites)|bower_components/,
+                     use: [
+                         {
+                             loader: 'babel-loader',
+                             options: Config.babel()
+                         }
+                     ]
+                 }
+             ]
+         }
+     })
    .sass('resources/assets/sass/app.scss', 'public/css');
