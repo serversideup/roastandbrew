91886fb6c6d44dfdf42ccfd6711153d17fd937a4
Jay Rogers <jay@521dimensions.com>
2019-01-16 11:56:35 -0600

    Added comments

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Pull code from Github
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p $DEPLOY_SSH_PORT -i /tmp/deploy_rsa $DEPLOY_SSH_USER@$DEPLOY_SERVER_HOST 'cd ~/httpdocs; git pull origin master'
+
+# Run migrations
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p $DEPLOY_SSH_PORT -i /tmp/deploy_rsa $DEPLOY_SSH_USER@$DEPLOY_SERVER_HOST 'cd ~/httpdocs; php artisan migrate'
